816dcea6b53166b2603fb82f5ed0817c22cab08a
Arclight151 <76915247+Arclight151@users.noreply.github.com>
2022-09-11 02:06:15 +0900

    Update Ch02_hw1_calculate.java

--- Project2/src/chap02/Ch02_hw1_calculate.java
+++ Project2/src/chap02/Ch02_hw1_calculate.java
@@ -5,7 +5,35 @@ import java.util.Scanner;
 public class Ch02_hw1_calculate {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		double result = 0;
+		
+		System.out.print("수식 입력: ");
+		int num1 = sc.nextInt();
+		String opr = sc.next();
+		int num2 = sc.nextInt();
+		
+		switch(opr) {
+		case("+"):
+			result = num1 + num2;
+			System.out.println("결과: " + num1 + opr + num2 + '=' + result);
+			break;
+		case("-"):
+			result = num1 - num2;
+			System.out.println("결과: " + num1 + opr + num2 + '=' + result);
+			break;
+		case("*"):
+			result = num1 * num2;
+			System.out.println("결과: " + num1 + opr + num2 + '=' + result);
+			break;
+		case("/"):
+			result = (double)num1 / num2;
+			System.out.println("결과: " + num1 + opr + num2 + '=' + result);
+			break;
+		default:
+			System.out.println("연산자 오류");
+		}
 
 	}
 
