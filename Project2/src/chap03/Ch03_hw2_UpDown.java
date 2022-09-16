8f67678b6bacc38d02450af8b3997e508cbb1500
Arclight151 <76915247+Arclight151@users.noreply.github.com>
2022-09-17 00:02:05 +0900

    Update Ch03_hw2_UpDown.java

--- Project2/src/chap03/Ch03_hw2_UpDown.java
+++ Project2/src/chap03/Ch03_hw2_UpDown.java
@@ -6,8 +6,39 @@ import java.util.Random;
 public class Ch03_hw2_UpDown {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
 		
+		int input = 100, r;
 		
+		while (true) {
+			r = rand.nextInt(100);
+			int attempt = 1;
+			
+			System.out.println("Guess the number(0~99), -1 = Quit");
+			while (input != r) {
+				System.out.print("Attempt " + attempt + ": ");
+				input = sc.nextInt();
+				if (input < 0) {
+					System.out.println("Quit");
+					break;
+				}
+				else if (input < r) {
+					System.out.println("Up");
+				}
+				else if (input > r) {
+					System.out.println("Down");
+				}
+				else
+					break;
+				attempt++;
+			}
+			if (input < 0)
+				break;
+			System.out.println("Correct");
+		}
+		
+		sc.close();
 	}
 
 }
