c2bacc7aa538d2de83409445b2a4b9718ed3b5bc
Arclight151 <76915247+Arclight151@users.noreply.github.com>
2022-10-11 16:48:33 +0900

    Update Play.java

--- Project2/src/chap05/game/play/Play.java
+++ Project2/src/chap05/game/play/Play.java
@@ -11,7 +11,7 @@ public class Play {
 	Bear br;
 	Fish fs;
 	
-	void initMap() {
+	private void initMap() {
 		for (int i = 0; i < mapX; i++) {
 			for (int j = 0; j < mapY; j++) {
 				map[i][j] = '-';
@@ -19,14 +19,58 @@ public class Play {
 		}
 	}
 	
-	public void start() {
-		while(gameState == INGAME) {
-			
+	private void updateMap() {
+		map[fs.gety()][fs.getx()] = fs.getIcon();
+		map[br.gety()][br.getx()] = br.getIcon();
+	}
+	
+	private void clearMap() {
+		map[br.gety()][br.getx()] = '-';
+		map[fs.gety()][fs.getx()] = '-';
+	}
+	
+	private void drawMap() {
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 15; j++) {
+				System.out.print(map[i][j]);
+			}
+			System.out.println();
 		}
+		System.out.println();
+	}
+	
+	public void start() {
+		System.out.println("Starting...");
+		initMap();
+		
+		br = new Bear((int)(Math.random() * mapY), (int)(Math.random() * mapX));
+		fs = new Fish(mapY / 2, mapX / 2);
+		
+		updateMap();
+		drawMap();
+		while(gameState == INGAME) {
+			clearMap();
+			br.move();
+			fs.move();
+			if (br.getx() == fs.getx() && br.gety() == fs.gety())
+				gameState = BEARWIN;
+			if (gameState != INGAME)
+				break;
+			updateMap();
+			drawMap();
+		}
+		if (gameState == BEARWIN) {
+			updateMap();
+			drawMap();
+			System.out.println("Fish Caught! Bear Wins!");
+		}
+		else
+			System.out.println("Fish Escaped! Fish Wins!");
 	}
 
 	public static void main(String[] args) {
-		
+		Play p = new Play();
+		p.start();
 
 	}
 
